da9a3a4eb6cec99d843c729e64c377a2e232381d
Ta Duc Hai K16_HL <haitdhe161580@fpt.edu.vn>
2023-06-26 09:56:11 +0700

    hai

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
@@ -6,6 +6,46 @@ import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
 import Container from "../components/Container";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Tạo một đối tượng contact mới từ thông tin người dùng
+    const newContact = {
+      
+      name,
+      email,
+      phone,
+      comment,
+    };
+
+    // Gửi yêu cầu POST để lưu dữ liệu mới
+    fetch("http://localhost:9999/contacts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newContact),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Dữ liệu đã được lưu:", data);
+      })
+      .catch((error) => {
+        console.error("Lỗi khi lưu dữ liệu:", error);
+      });
+
+    // Xóa nội dung trong các trường sau khi lưu thành công
+    setName("");
+    setEmail("");
+    setPhone("");
+    setComment("");
+  };
+
   return (
     <>
       <Meta title={"Contact Us"} />
@@ -13,7 +53,7 @@ const Contact = () => {
       <Container class1="contact-wrapper py-5 home-wrapper-2">
         <div className="row">
           <div className="col-12">
-            <iframe
+          <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6986.771103663534!2d76.99275607711007!3d28.886888929272477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390da5e51463d4c9%3A0xe5a485e2ac7c3d4a!2sMandaura%2C%20Haryana%20131103!5e0!3m2!1sen!2sin!4v1669909087902!5m2!1sen!2sin"
               width="600"
               height="450"
@@ -22,49 +62,56 @@ const Contact = () => {
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
-          </div>
-          <div className="col-12 mt-5">
-            <div className="contact-inner-wrapper d-flex justify-content-between ">
+            </div>
+            <div className="col-6 mt-5">
+            <form onSubmit={handleSubmit} className="d-flex flex-column gap-15">
               <div>
-                <h3 className="contact-title mb-4">Contact</h3>
-                <form action="" className="d-flex flex-column gap-15">
-                  <div>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div>
-                    <input
-                      type="email"
-                      className="form-control"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div>
-                    <input
-                      type="tel"
-                      className="form-control"
-                      placeholder="Mobile Number"
-                    />
-                  </div>
-                  <div>
-                    <textarea
-                      name=""
-                      id=""
-                      className="w-100 form-control"
-                      cols="30"
-                      rows="4"
-                      placeholder="Comments"
-                    ></textarea>
-                  </div>
-                  <div>
-                    <button className="button border-0">Submit</button>
-                  </div>
-                </form>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
               <div>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="tel"
+                  className="form-control"
+                  placeholder="Mobile Number"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <div>
+                <textarea
+                  name=""
+                  id=""
+                  className="w-100 form-control"
+                  cols="30"
+                  rows="4"
+                  placeholder="Comments"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                ></textarea>
+              </div>
+              <div>
+                <button type="submit" className="button border-0">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+          <div  className="col-6 mt-5">
                 <h3 className="contact-title mb-4">Get in touch with us</h3>
                 <div>
                   <ul className="ps-0">
@@ -92,8 +139,6 @@ const Contact = () => {
                   </ul>
                 </div>
               </div>
-            </div>
-          </div>
         </div>
       </Container>
     </>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,57 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+
 const Signup = () => {
+  const navigate = useNavigate()
+  const handleSignUp = async (event) => {
+
+    event.preventDefault();
+    const id = event.target.email.value;
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const mobile = event.target.mobile.value;
+    const password = event.target.password.value;
+
+    // Tạo đối tượng user mới
+    const newUser = {
+      id,
+      name,
+      email,
+      phone: mobile,
+      password,
+      role:"Customer"
+    };
+
+    try {
+      // Gửi yêu cầu đăng ký đến API backend
+      const response = await fetch("http://localhost:9999/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
+
+      if (response.ok) {
+        console.log(response.data);
+
+        alert("Đăng ký thành công!");
+        navigate("/login");
+        // Các xử lý khác sau khi đăng ký thành công
+      } else {
+        throw new Error("Đăng ký thất bại.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Đã xảy ra lỗi. Vui lòng thử lại sau.");
+      // Các xử lý khác khi xảy ra lỗi
+    }
+  };
+
   return (
     <>
       <Meta title={"Sign Up"} />
@@ -14,7 +61,7 @@ const Signup = () => {
           <div className="col-12">
             <div className="auth-card">
               <h3 className="text-center mb-3">Sign Up</h3>
-              <form action="" className="d-flex flex-column gap-15">
+              <form onSubmit={handleSignUp} className="d-flex flex-column gap-15">
                 <CustomInput type="text" name="name" placeholder="Name" />
                 <CustomInput type="email" name="email" placeholder="Email" />
                 <CustomInput
@@ -29,7 +76,7 @@ const Signup = () => {
                 />
                 <div>
                   <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                    <button className="button border-0">Sign Up</button>
+                    <button type="submit" className="button border-0">Sign Up</button>
                   </div>
                 </div>
               </form>
